52b29b7a996f65802f6d148df98309e8588f48ec
Havrylenko Volodymyr <gvs88986@gmail.com>
2025-08-09 04:12:06 +0300

    buildSuccess

--- src/main/java/com/example/springbootapplication/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/springbootapplication/dto/CreateBookRequestDto.java
@@ -1,14 +1,35 @@
 package com.example.springbootapplication.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank(message = "Book title is required")
+    @Size(max = 255, message = "Book title must not exceed 255 characters")
     private String title;
+
+    @Size(max = 255, message = "Author name must not exceed 255 characters")
     private String author;
+
+    @Pattern(regexp = "^(97(8|9))?\\d{9}(\\d|X)$",
+            message = "Invalid ISBN format")
     private String isbn;
+
+    @NotNull(message = "Price is required")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than 0")
+    @Digits(integer = 10, fraction = 2, message = "Invalid price format")
     private BigDecimal price;
+
+    @Size(max = 2000, message = "Description must not exceed 2000 characters")
     private String description;
+
+    @Size(max = 512, message = "Cover image URL must not exceed 512 characters")
     private String coverImage;
 }
