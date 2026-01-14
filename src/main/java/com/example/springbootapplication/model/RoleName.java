7d5e23b2c16fb271b8d3ecca36f6ac019f3d1fdd
Volodymyr Havrylenko <gvs88986@gmail.com>
2026-01-14 15:59:07 +0200

    buildSuccess

---
+++ src/main/java/com/example/springbootapplication/model/RoleName.java
@@ -0,0 +1,6 @@
+package com.example.springbootapplication.model;
+
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
--- src/main/java/com/example/springbootapplication/service/user/UserServiceImpl.java
+++ src/main/java/com/example/springbootapplication/service/user/UserServiceImpl.java
@@ -4,10 +4,16 @@ import com.example.springbootapplication.dto.user.UserRegistrationRequestDto;
 import com.example.springbootapplication.dto.user.UserResponseDto;
 import com.example.springbootapplication.exception.RegistrationException;
 import com.example.springbootapplication.mapper.UserMapper;
+import com.example.springbootapplication.model.Role;
+import com.example.springbootapplication.model.RoleName;
 import com.example.springbootapplication.model.User;
+import com.example.springbootapplication.repository.RoleRepository;
 import com.example.springbootapplication.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -15,15 +21,28 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
-    public UserResponseDto register(UserRegistrationRequestDto requestDto)
+    public UserResponseDto getByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .map(userMapper::toUserResponse)
+                .orElseThrow(() -> new RuntimeException("Can't find user"));
+    }
+
+    @Override
+    public UserResponseDto register(UserRegistrationRequestDto request)
             throws RegistrationException {
-        if (userRepository.existsByEmail(requestDto.getEmail())) {
-            throw new RegistrationException("Email already in use: " + requestDto.getEmail());
+        if(userRepository.findByEmail(request.getEmail()).isPresent()) {
+            throw new RegistrationException("Email already in use: " + request.getEmail());
         }
-        User user = userMapper.toModel(requestDto);
-        userRepository.save(user);
-        return userMapper.toUserResponse(user);
+        User user = userMapper.toModel(request);
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new RegistrationException("Can't find role by name: " + RoleName.ROLE_USER));
+        user.setRoles(Set.of(userRole));
+        User savedUser = userRepository.save(user);
+        return userMapper.toUserResponse(savedUser);
     }
 }
