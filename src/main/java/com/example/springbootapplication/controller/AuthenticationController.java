ef164678c209c8c46a9ceb73e487cb311dcb1f8a
Volodymyr Havrylenko <gvs88986@gmail.com>
2026-02-06 12:03:00 +0200

    buildSuccess_06.02.26

--- src/main/java/com/example/springbootapplication/controller/AuthenticationController.java
+++ src/main/java/com/example/springbootapplication/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.example.springbootapplication.controller;
 
+import com.example.springbootapplication.dto.user.UserLoginRequestDto;
+import com.example.springbootapplication.dto.user.UserLoginResponseDto;
 import com.example.springbootapplication.dto.user.UserRegistrationRequestDto;
 import com.example.springbootapplication.dto.user.UserResponseDto;
 import com.example.springbootapplication.exception.RegistrationException;
+import com.example.springbootapplication.security.AuthenticationService;
 import com.example.springbootapplication.service.user.UserServiceImpl;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -16,10 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final UserServiceImpl userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     public UserResponseDto registerUser(@Valid @RequestBody UserRegistrationRequestDto requestDto)
             throws RegistrationException {
         return userService.register(requestDto);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
