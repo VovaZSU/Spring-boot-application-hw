eb807e814776e66d88cf284187fc169a82390518
Volodymyr Havrylenko <gvs88986@gmail.com>
2026-02-07 19:13:56 +0200

    commentsСorrected_07.02

--- src/main/java/com/example/springbootapplication/dto/user/UserLoginRequestDto.java
+++ src/main/java/com/example/springbootapplication/dto/user/UserLoginRequestDto.java
@@ -1,15 +1,16 @@
 package com.example.springbootapplication.dto.user;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
 
 public record UserLoginRequestDto(
-        @NotEmpty
+        @NotBlank
         @Length(min = 8, max = 20)
         @Email
         String email,
-        @NotEmpty
+
+        @NotBlank
         @Length(min = 4, max = 20)
         String password
 ) {
